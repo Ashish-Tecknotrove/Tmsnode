43c361598fc1068a9ff1911e9f52241eff50cbfc
Ashish-Tecknotrove <97014213+Ashish-Tecknotrove@users.noreply.github.com>
2022-01-03 12:45:47 +0530

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,37 @@
+import dotenv  from "dotenv";
+import express from "express";
+
+import bodyParser from "body-parser";
+
+import root_route from "./src/routes/root/root_routes" ;
+import curriculumroutes from "./src/routes/root/curriculumroutes";
+import Languages from "./src/model/language/language.model";
+import LanguageRoutes from "./src/routes/language/language.routes";
+
+
+var multer = require('multer');
+var upload = multer();
+
+const app = express();
+const form = multer();
+
+dotenv.config();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(upload.array()); 
+app.use(express.static('public'));
+
+app.listen(process.env.port,()=>{
+    console.log("Node Server Started Running")
+});
+
+app.use("/TMS",root_route);
+app.use("/TMS",curriculumroutes);
+// app.use("/api/v1/",LanguageRoutes)
+
+app.get('/',(req,res)=>{
+    res.status(200).json({message:"Welcome To TMS"});
+});
