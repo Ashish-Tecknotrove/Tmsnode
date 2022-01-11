0dde11b8083f348cfd1a48153ee0c91e69ff08fe
Ashish-Tecknotrove <97014213+Ashish-Tecknotrove@users.noreply.github.com>
2022-01-11 13:30:29 +0530

    Update app.js

--- dist/app.js
+++ dist/app.js
@@ -1,38 +1,31 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const dotenv = __importStar(require("dotenv"));
+const dotenv_1 = __importDefault(require("dotenv"));
 const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const root_routes_1 = __importDefault(require("./src/routes/root/root_routes"));
+const curriculumroutes_1 = __importDefault(require("./src/routes/root/curriculumroutes"));
+var multer = require('multer');
+var cors = require('cors');
+var upload = multer();
 const app = (0, express_1.default)();
-dotenv.config();
+const form = multer();
+dotenv_1.default.config();
 app.use(body_parser_1.default.json());
-app.listen(process.env.port, () => {
+app.use(body_parser_1.default.urlencoded());
+app.use(body_parser_1.default.urlencoded({ extended: true }));
+app.use(cors());
+app.use(upload.array());
+app.use(express_1.default.static('public'));
+app.listen(process.env.PORT || 3000, () => {
     console.log("Node Server Started Running");
 });
 app.use("/TMS", root_routes_1.default);
+app.use("/TMS", curriculumroutes_1.default);
+// app.use("/api/v1/",LanguageRoutes)
 app.get('/', (req, res) => {
     res.status(200).json({ message: "Welcome To TMS" });
 });
