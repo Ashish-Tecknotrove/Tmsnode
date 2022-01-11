fb1795393e61ab178d3f26f7d6c7416f47c13591
Ashish-Tecknotrove <97014213+Ashish-Tecknotrove@users.noreply.github.com>
2022-01-11 14:22:29 +0530

    Update app.js

--- dist/app.js
+++ dist/app.js
@@ -3,29 +3,32 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const dotenv_1 = __importDefault(require("dotenv"));
 const express_1 = __importDefault(require("express"));
-const body_parser_1 = __importDefault(require("body-parser"));
-const root_routes_1 = __importDefault(require("./src/routes/root/root_routes"));
-const curriculumroutes_1 = __importDefault(require("./src/routes/root/curriculumroutes"));
-var multer = require('multer');
-var cors = require('cors');
-var upload = multer();
+// import bodyParser from "body-parser";
+//
+// import root_route from "./src/routes/root/root_routes" ;
+// import curriculumroutes from "./src/routes/root/curriculumroutes";
+// import Languages from "./src/model/language/language.model";
+// import LanguageRoutes from "./src/routes/language/language.routes";
+// var multer = require('multer');
+// var cors = require('cors')
+// var upload = multer();
 const app = (0, express_1.default)();
-const form = multer();
-dotenv_1.default.config();
-app.use(body_parser_1.default.json());
-app.use(body_parser_1.default.urlencoded());
-app.use(body_parser_1.default.urlencoded({ extended: true }));
-app.use(cors());
-app.use(upload.array());
+// const form = multer();
+// dotenv.config();
+//
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded());
+// app.use(bodyParser.urlencoded({extended:true}));
+// app.use(cors())
+// app.use(upload.array());
 app.use(express_1.default.static('public'));
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 8000, () => {
     console.log("Node Server Started Running");
 });
-app.use("/TMS", root_routes_1.default);
-app.use("/TMS", curriculumroutes_1.default);
-// app.use("/api/v1/",LanguageRoutes)
+// app.use("/TMS",root_route);
+// app.use("/TMS",curriculumroutes);
+// // app.use("/api/v1/",LanguageRoutes)
 app.get('/', (req, res) => {
     res.status(200).json({ message: "Welcome To TMS" });
 });
