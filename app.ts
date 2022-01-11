018363483d9793ea1d026fb8d4a0acbce86c5ceb
Ashish-Tecknotrove <97014213+Ashish-Tecknotrove@users.noreply.github.com>
2022-01-11 14:19:20 +0530

    Update app.ts

--- app.ts
+++ app.ts
@@ -28,8 +28,8 @@ app.use(express.static('public'));
 //     console.log("Node Server Started Running")
 // });
 
-app.listen(process.env.PORT || 5000), function(){
-  console.log('listening on *:5000');
+app.listen(process.env.PORT || 3000), function(){
+  console.log('listening on *:3000');
 });
 
 app.use("/TMS",root_route);
