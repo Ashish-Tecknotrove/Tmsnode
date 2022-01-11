b5eaff85c3ef09ccb205c4dc5bbd3e32a910e3d4
Ashish-Tecknotrove <97014213+Ashish-Tecknotrove@users.noreply.github.com>
2022-01-11 12:34:45 +0530

    Update app.ts

--- app.ts
+++ app.ts
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(upload.array()); 
 app.use(express.static('public'));
 
-app.listen(process.env.port,()=>{
+app.listen(process.env.PORT || 3000,()=>{
     console.log("Node Server Started Running")
 });
 
