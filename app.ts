9b537ca44592447c194002c2393233eebe2fddc8
Ashish-Tecknotrove <97014213+Ashish-Tecknotrove@users.noreply.github.com>
2022-01-11 14:21:57 +0530

    Update app.ts

--- app.ts
+++ app.ts
@@ -1,41 +1,39 @@
 import dotenv  from "dotenv";
 import express from "express";
 
-import bodyParser from "body-parser";
+// import bodyParser from "body-parser";
+//
+// import root_route from "./src/routes/root/root_routes" ;
+// import curriculumroutes from "./src/routes/root/curriculumroutes";
+// import Languages from "./src/model/language/language.model";
+// import LanguageRoutes from "./src/routes/language/language.routes";
 
-import root_route from "./src/routes/root/root_routes" ;
-import curriculumroutes from "./src/routes/root/curriculumroutes";
-import Languages from "./src/model/language/language.model";
-import LanguageRoutes from "./src/routes/language/language.routes";
 
-
-var multer = require('multer');
-var upload = multer();
+// var multer = require('multer');
+// var cors = require('cors')
+// var upload = multer();
 
 const app = express();
-const form = multer();
+// const form = multer();
 
-dotenv.config();
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(bodyParser.urlencoded({extended:true}));
-
-app.use(upload.array()); 
+// dotenv.config();
+//
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded());
+// app.use(bodyParser.urlencoded({extended:true}));
+// app.use(cors())
+// app.use(upload.array());
 app.use(express.static('public'));
 
-// app.listen(process.env.PORT || 3000,()=>{
-//     console.log("Node Server Started Running")
-// });
-
-app.listen(process.env.PORT || 3000), function(){
-  console.log('listening on *:3000');
+app.listen(process.env.PORT || 8000,()=>{
+    console.log("Node Server Started Running")
 });
 
-app.use("/TMS",root_route);
-app.use("/TMS",curriculumroutes);
-// app.use("/api/v1/",LanguageRoutes)
+// app.use("/TMS",root_route);
+// app.use("/TMS",curriculumroutes);
+// // app.use("/api/v1/",LanguageRoutes)
 
 app.get('/',(req,res)=>{
     res.status(200).json({message:"Welcome To TMS"});
 });
+
