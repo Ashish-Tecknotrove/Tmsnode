41babdf426a6e8e7355ddb55de08835bc2e56562
Ashish-Tecknotrove <97014213+Ashish-Tecknotrove@users.noreply.github.com>
2022-01-15 17:34:58 +0530

    Add files via upload

--- app.ts
+++ app.ts
@@ -3,24 +3,26 @@ import express from "express";
 import bodyParser from "body-parser";
 import root_route from "./src/routes/root/root_routes" ;
 import curriculumroutes from "./src/routes/root/curriculumroutes";
+import companyroutes from "./src/routes/root/companyroutes";
+import subscriptionroutes from "./src/routes/root/subscriptionroutes";
 import Languages from "./src/model/language/language.model";
 import LanguageRoutes from "./src/routes/language/language.routes";
 
 
 var multer = require('multer');
-var cors = require('cors')
-var upload = multer();
+var formData = multer();
+var cors = require('cors');
 
 const app = express();
-const form = multer();
 
 dotenv.config();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(cors())
-app.use(upload.array());
+//app.use(bodyParser());
+app.use(bodyParser.json()); // For Accepting the JSON Data
+app.use(bodyParser.urlencoded()); // For Accepting the x-www-form-Data
+app.use(bodyParser.urlencoded({extended:true }));
+app.use(cors());
+//app.use(formdata.array());
 app.use(express.static('public'));
 
 app.listen(process.env.PORT || 8000,()=>{
@@ -29,8 +31,12 @@ app.listen(process.env.PORT || 8000,()=>{
 
 app.use("/TMS",root_route);
 app.use("/TMS",curriculumroutes);
+app.use("/TMS",companyroutes);
+app.use("/TMS/subscription",subscriptionroutes);
 // app.use("/api/v1/",LanguageRoutes)
 
 app.get('/',(req,res)=>{
     res.status(200).json({message:"Welcome To TMS"});
 });
+
+export default app;
