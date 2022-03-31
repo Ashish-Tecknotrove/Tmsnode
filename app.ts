8afe545be694567082191d73bd80dd92417b41a9
Ashish-Tecknotrove <97014213+Ashish-Tecknotrove@users.noreply.github.com>
2022-03-31 15:26:14 +0530

    Add files via upload

--- app.ts
+++ app.ts
@@ -1,29 +1,25 @@
 import dotenv  from "dotenv";
 import express from "express";
 import bodyParser from "body-parser";
-import root_route from "./src/routes/root/root_routes" ;
-import curriculumroutes from "./src/routes/root/curriculumroutes";
-import companyroutes from "./src/routes/root/companyroutes";
-import subscriptionroutes from "./src/routes/root/subscriptionroutes";
-import elearningroutes from "./src/routes/elearning/elearningcontentroutes";
-import Languages from "./src/model/language/language.model";
-import LanguageRoutes from "./src/routes/language/language.routes";
-import TraineeRoutes from './src/routes/root/traineeroutes';
 import morgan from "morgan";
-import trainerRoutes from "./src/routes/root/trainerroutes";
-import subcompany from "./src/routes/root/subcompanyroutes";
+import authRoutes from "./src/auth/authentication.routes";
+import SuperAdminRoutes from "./src/app/superadmin/routes/root/superadmin.routes";
+import super_admin_elearning_router from "./src/app/superadmin/routes/elearning/superadmin.elearningcontent.routes";
+import ElearningTraineeRoutes from "./src/app/trainee/routes/elearning/trainee.elearning.routes";
+import TraineeRootRoutes from "./src/app/trainee/routes/root/trainee.root.routes";
+import resources from "./src/resources/additionalresources.routes";
+import CompanyAdminRoutes from "./src/app/companyadmin/routes/root/company_admin.routes";
 
-import master_departmentRoutes from "./src/routes/root/master_departmentroutes";
-import CompanyDepartment from "./src/routes/root/company_departmentroutes";
-import ElearningTraineeRoutes from "./src/routes/elearning/elearning.traineeroutes";
-import simulatorRoutes from "./src/routes/root/simulatorRoutes";
+import TrainerRoutes from "./src/app/trainer/routes/root/trainer.routes";
+
+
 
 process.env.TZ = "Asia/Calcutta";
 const nDate = new Date().toLocaleString('en-US', {
     timeZone: 'Asia/Calcutta'
     });
 
-    console.log(nDate); 
+console.log(nDate);
     
 var multer = require('multer');
 var formData = multer();
@@ -33,38 +29,51 @@ const app = express();
 
 dotenv.config();
 
-//app.use(bodyParser());
+//controller.use(bodyParser());
 app.use(bodyParser.json()); // For Accepting the JSON Data
 app.use(bodyParser.urlencoded()); // For Accepting the x-www-form-Data
 app.use(bodyParser.urlencoded({extended:true }));
 app.use(cors());
-//app.use(formdata.array());
+//controller.use(formdata.array());
 app.use('/resources',express.static('resources'));
 
 app.listen(process.env.PORT || 8000,()=>{
     console.log("Node Server Started Running")
 });
 
-app.use("/TMS",root_route);
-app.use("/TMS",curriculumroutes);
-app.use("/TMS",companyroutes);
-app.use("/TMS/subscription",subscriptionroutes);
-app.use("/TMS/elearning",elearningroutes);
-app.use("/TMS",LanguageRoutes)
-app.use("/TMS",TraineeRoutes)
-app.use("/TMS/companyadmin",trainerRoutes);
-app.use("/TMS/department",master_departmentRoutes);
-app.use("/TMS/department",CompanyDepartment);
-app.use("/TMS/subcompany",subcompany),
+app.disable('x-powered-by');
 
-app.use("/TMS/trainee/elearning",ElearningTraineeRoutes)
-app.use("/TMS/simulator",simulatorRoutes)
+
+//TODO ROUTES
+app.use("/TMS/auth",authRoutes);
+app.use("/TMS/resources",resources)
+
+//TODO SUPER ADMIN ROUTES
+app.use("/TMS/superadmin",SuperAdminRoutes);
+app.use("/TMS/superadmin/elearning",super_admin_elearning_router);
+//TODO SUPER ADMIN ROUTES ENDS
+
+//TODO TRAINEE ROUTES
+app.use("/TMS/trainee",TraineeRootRoutes)
+app.use("/TMS/trainee/elearning",ElearningTraineeRoutes);
+
+//TODO TRAINEE ROUTES --END
+
+//TODO COMPANY ADMIN ROUTES
+app.use("/TMS/companyadmin",CompanyAdminRoutes);
+//TODO COMPANY ADMIN ROUTES --END
+
+//TODO TRAINER ROUTES
+app.use("/TMS/trainer",TrainerRoutes);
+//TODO TRAINER ROUTES --END
+
+
+
 app.use(express.urlencoded({extended: true}));
 app.use(morgan('dev'));
 
-app.use(express.static(__dirname));
 
-app.enable('trust proxy');
+app.use(express.static(__dirname));
 
 app.get('/',(req,res)=>{
 
